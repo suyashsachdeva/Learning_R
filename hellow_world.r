57c7357f76524c664a092f9fd5fae307c7fbddce
Suyash Sachdeva <77083945+suyashsachdeva@users.noreply.github.com>
2024-11-12 23:55:48 +0530

    Hello world

---
+++ hellow_world.r
@@ -0,0 +1,12 @@
+print("Hello World this is R")
+# dplyr: manupulating data frames
+# tidyr: cleaning of teh data 
+# stringt: strings and text information manupulation 
+# lubridate: date information 
+# htrr: for websites 
+# ggvis:  grammer of graphs interaction
+# ggplot2: data visualization 
+# shiny: interactive applixations on websites 
+# rio: input/output
+# rmarkdown: interactivae notebooks 
+# pacman: p[ackage manager
